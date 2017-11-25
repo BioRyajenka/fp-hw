190f58c832e7d0e8ab467f2f6fa0ca319176f5b9
BioRyajenka <ism@veeroute.com>
2017-11-26 00:21:06 +0300

    Implement 1st part with monads

--- hw3/src/Task1.hs
+++ hw3/src/Task1.hs
@@ -1,22 +1,33 @@
 module Task1
-       ( Expr
-
+       ( Expr(..),
+         ArithmeticError(..),
+         exec
        ) where
 
-import qualified Data.Map as Map
+import           Control.Monad.Reader
+import qualified Data.Map             as Map
 
-data Expr = Lit Int
-  | Var String
+data Expr =
+    Var String
   | Expr `Add` Expr
   | Expr `Mul` Expr
   | Expr `Div` Expr
   | Let String Expr Expr
 
-exec :: Expr -> Maybe Int
-exec = execWithMap Map.empty where
-    {- [(String, Maybe Int)] -}
-    execWithMap :: Map.Map String Int -> Expr -> Maybe Int
-    execWithMap varsMap (Var varname) = Map.lookup varname varsMap
-    execWithMap varsMap (a `Add` b) = fmap (+) (execWithMap varsMap a) <*> (execWithMap varsMap b)
-    execWithMap varsMap (a `Mul` b) = fmap (*) (execWithMap varsMap a) <*> (execWithMap varsMap b)
-    execWithMap varsMap (a `Div` b) = fmap quot (execWithMap varsMap a) <*> (execWithMap varsMap b)
+data ArithmeticError = ArithmeticError
+
+maybeToEither :: e -> Maybe a -> Either e a
+maybeToEither _ (Just a) = Right a
+maybeToEither e Nothing  = Left e
+
+exec :: Expr -> Reader (Map.Map String Integer) (Either ArithmeticError Integer)
+exec expr = reader $ \varsMap -> execWithMap varsMap expr where
+    execWithMap :: Map.Map String Integer -> Expr -> Either ArithmeticError Integer
+    execWithMap varsMap (Var varname) = maybeToEither ArithmeticError $ Map.lookup varname varsMap
+    execWithMap varsMap (a `Add` b) = fmap (+) (execWithMap varsMap a) <*> execWithMap varsMap b
+    execWithMap varsMap (a `Mul` b) = fmap (*) (execWithMap varsMap a) <*> execWithMap varsMap b
+    execWithMap varsMap (a `Div` b) = fmap quot (execWithMap varsMap a) <*> execWithMap varsMap b
+    execWithMap varsMap (Let varname what inwhere) =
+      if Map.member varname varsMap
+      then Left ArithmeticError
+      else execWithMap varsMap what >>= \newwhat -> execWithMap (Map.insert varname newwhat varsMap) inwhere
